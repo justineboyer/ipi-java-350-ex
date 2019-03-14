4c6d010c2492fd813b807afcd316a57c07353b6c
justineBoyer <boyer.just@gmail.com>
2019-03-14 15:34:15 +0100

    tests

--- src/test/java/com/ipiecole/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecole/java/java350/model/EmployeTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 public class EmployeTest {
-    @Test
 
+    @Test
     public void testGetNombreAnneeAncienneteNow () {
         // Given
         Employe employe = new Employe(); // J'initialise un employé
@@ -17,6 +17,48 @@ public class EmployeTest {
 
         // When
         Integer nbAnnee = employe.getNombreAnneeAnciennete(); // Je récupère le nombre d'années dans une variable avec la méthode. Cette méthode repose sur une instance donc on doit instancier un employé dans les données d'entrée
+
+        // Then
+        // nbAnnee = 0 // Je vérifie que cette date soit égale à la valeur attendue
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteNull () {
+        // Given
+        Employe e = new Employe(); // J'initialise un employé
+        e.setDateEmbauche(null); // Je dois stocker cette date dans l'employé
+
+        // When
+        Integer nbAnnee = e.getNombreAnneeAnciennete(); // Je récupère le nombre d'années dans une variable avec la méthode. Cette méthode repose sur une instance donc on doit instancier un employé dans les données d'entrée
+
+        // Then
+        // nbAnnee = 0 // Je vérifie que cette date soit égale à la valeur attendue
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteNMoins2 () {
+        // Given
+        Employe e = new Employe(); // J'initialise un employé
+        e.setDateEmbauche(LocalDate.now().minusYears(2)); // Je dois stocker cette date dans l'employé
+
+        // When
+        Integer nbAnnee = e.getNombreAnneeAnciennete(); // Je récupère le nombre d'années dans une variable avec la méthode. Cette méthode repose sur une instance donc on doit instancier un employé dans les données d'entrée
+
+        // Then
+        // nbAnnee = 0 // Je vérifie que cette date soit égale à la valeur attendue
+        Assertions.assertThat(nbAnnee).isEqualTo(2);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteNPlus2 () {
+        // Given
+        Employe e = new Employe(); // J'initialise un employé
+        e.setDateEmbauche(LocalDate.now().plusYears(2)); // Je dois stocker cette date dans l'employé
+
+        // When
+        Integer nbAnnee = e.getNombreAnneeAnciennete(); // Je récupère le nombre d'années dans une variable avec la méthode. Cette méthode repose sur une instance donc on doit instancier un employé dans les données d'entrée
 
         // Then
         // nbAnnee = 0 // Je vérifie que cette date soit égale à la valeur attendue
